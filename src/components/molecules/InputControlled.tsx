cc2e6e3c5b901116c3166325c21b507a0bbd228d
Jchan <gkswocks123@gmail.com>
2023-02-04 12:00:59 +0900

    ✨ Feat : InputControlled 컴포넌트 생성 - MUI와 react-hook-form을 사용하기 위해 useController를 이용해 컴포넌트 생성

---
+++ src/components/molecules/InputControlled.tsx
@@ -0,0 +1,17 @@
+import { TextField, TextFieldProps } from "@mui/material";
+import { useController, UseControllerProps } from "react-hook-form";
+
+type ControlledMuiProps = TextFieldProps & { useControllerProps: UseControllerProps };
+
+/**
+ * @description
+ * 제어 컴포넌트를 위해 useController를 이용해 Input 생성
+ */
+export const InputControlled = ({ useControllerProps, ...props }: ControlledMuiProps) => {
+  const {
+    field,
+    fieldState: { error },
+  } = useController(useControllerProps);
+
+  return <TextField {...field} {...props} error={!!error} helperText={!!error && error.message} />;
+};
