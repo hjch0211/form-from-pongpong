96087365a7cd96250dcc4bf13533391130a639df
Jchan <gkswocks123@gmail.com>
2023-02-06 20:52:08 +0900

    ✨ Feat : SignIn 페이지 라우트 설정

--- src/pages/MyRoutes.tsx
+++ src/pages/MyRoutes.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes as ReactRoutes } from "react-router-dom";
 import { Main } from "./Main";
+import { SignIn } from "./sign/SignIn";
 
 export const MyRoutes = () => {
   return (
     <ReactRoutes>
       <Route path="/" element={<Main />} />
+      <Route path="/sign-in" element={<SignIn />} />
     </ReactRoutes>
   );
 };
