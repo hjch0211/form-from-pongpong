d16a3bd476856e3a1355fa6cf3c4f8ee9232ff7a
Jchan <gkswocks123@gmail.com>
2023-02-14 14:10:04 +0900

    ✨ Feat : SignUp 페이지 생성

--- src/pages/MyRoutes.tsx
+++ src/pages/MyRoutes.tsx
@@ -1,12 +1,16 @@
 import { Route, Routes as ReactRoutes } from "react-router-dom";
+
+// Pages
 import { Main } from "./Main";
 import { SignIn } from "./sign/SignIn";
+import { SignUp } from "./sign/SignUp";
 
 export const MyRoutes = () => {
   return (
     <ReactRoutes>
       <Route path="/" element={<Main />} />
       <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-up" element={<SignUp />} />
     </ReactRoutes>
   );
 };
---
+++ src/pages/sign/SignUp/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { MainHeader } from "../../../components/organisms";
+import { MainLayout } from "../../../components/templates/MainLayout";
+
+export const SignUp = () => {
+  return (
+    <>
+      <MainHeader />
+      <MainLayout>{}</MainLayout>
+    </>
+  );
+};
